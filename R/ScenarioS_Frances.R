75836cdeb37227c336b73d2f6daf398a93d3cdd9
StewartResearch <fecstewart@gmail.com>
2019-04-23 13:32:48 -0700

    Updated ScenarioS_F code

--- R/ScenarioS_Frances.R
+++ R/ScenarioS_Frances.R
@@ -1,11 +1,11 @@
-MCRUNS_F<-function (Area, Regime, IND = 50, CF = 1, Density = 0.06) 
+SenarioS_F<-function (Area, Regime, IND = 50, CF = 1, Density = 0.04) 
 {
   InitPop <- function(K) {
     aoy = round(K * 0.6769521)
     yoy = K - aoy
     matrix(c(aoy, yoy), nrow = 2, ncol = 1, byrow = TRUE)
   }
-  K <- round(Area/100)*(Density * 0.5)[1]
+  K <- round(Area * Density * 0.5)[1]
   P0 <- InitPop(K)
   print(P0)
   tmp <- FireModelS(Regime = Regime * CF, N = 400, T = 200, 
@@ -13,14 +13,14 @@ MCRUNS_F<-function (Area, Regime, IND = 50, CF = 1, Density = 0.06)
   pYoung1 <- tmp$Young
   AS1 <- tmp$EndForest
   fp1 <- rep(0, length(pYoung1))
-  Stage1 <- Caribou_F(K = K, p50s = pYoung1, hoof = fp1, Pop = P0)
+  Stage1 <- Caribou(K = K, p50s = pYoung1, hoof = fp1, Pop = P0)
   print(paste("EndPop1 = ", Stage1$EndPop, sep = ""))
   tmp2 <- FireModelS(Regime = Regime, N = 400, T = 200, AgeStruct = AS1, 
                      SimFire = FALSE)
   pYoung2 <- tmp2$Young
   AS2 <- tmp2$EndForest
   fp2 <- IND
-  Stage2 <- Caribou_F(K = K, p50s = pYoung2, hoof = fp2, Pop = Stage1$EndPop)
+  Stage2 <- Caribou(K = K, p50s = pYoung2, hoof = fp2, Pop = Stage1$EndPop)
   print(paste("EndPop2 = ", Stage2$EndPop, sep = ""))
   CurRegime <- rev(Regime)[1:length(Regime)]
   tmp3 <- FireModelS(CurRegime, N = 50, T = 200, AgeStruct = AS2, 
@@ -28,7 +28,7 @@ MCRUNS_F<-function (Area, Regime, IND = 50, CF = 1, Density = 0.06)
   pYoung3 <- tmp3$Young
   fp3 <- rep(tail(IND, 1), length(pYoung3))
   n <- length(pYoung2)
-  Stage3 <- Caribou_F(K = K, p50s = pYoung3, hoof = fp3, Pop = Stage2$EndPop)
+  Stage3 <- Caribou(K = K, p50s = pYoung3, hoof = fp3, Pop = Stage2$EndPop)
   print(paste("EndPop3 = ", Stage3$EndPop, sep = ""))
   foo <- c(Stage1$Lambda, Stage2$Lambda, Stage3$Lambda)
   plot(foo, type = "l", ylab = "Lambda", xlab = "Year")
