f267d4b28e418b9f6ab3e2dfc17fdcb2a9e38ee1
StewartResearch <fecstewart@gmail.com>
2019-06-17 16:15:22 -0700

    Getting CIs for average lambda < 1.0 dates

---
+++ R/LambdaDates.R
@@ -0,0 +1,15 @@
+# Make a vector of all the years where a lambda < 1 showed showed up within the simulations
+
+LambdaDates <- function(SimulationRun) {
+  
+  #SmallL <- SimulationRun < 1.0
+  # rows = 300 repetitions from MCRUNS
+  # columns = 239 years (1818-2057)
+
+  extinctYear <- data.table::rbindlist(lapply(X = 1:ncol(SimulationRun), FUN = function(year){
+    LambdaLess <- any(SimulationRun[,year] < 1.0)
+    extTable <- data.frame(Year = 1818 + year, Lambda = LambdaLess)
+    return(extTable)
+  }))
+  return(extinctYear)
+}
