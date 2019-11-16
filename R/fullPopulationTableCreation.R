bf4dc61d177810535655ad0f104178ea948c54ea
StewartResearch <fecstewart@gmail.com>
2019-11-15 16:49:49 -0800

    Repeating functions for making the "Caribou plot" but altering them for making a N plot

---
+++ R/fullPopulationTableCreation.R
@@ -0,0 +1,55 @@
+fullPopulationTableCreation <- function(dataPathGDrive = "https://drive.google.com/open?id=1avEapiUArQx28paSz28WlWrT69fZ7hf1", 
+                              # Full path to data hosted in google drive. Should be a .zip file of all .rds files from the herds 
+                              dataName = file.path(getwd(), "data", "lambdaTableAllScenarios_V3"), # full path to data to be saved without extension
+                              typeOfFile = "csv", # type of file to save, currently supports: "csv" or "rds"
+                              uploadToGoogleDrive = FALSE){
+  library("reproducible")
+  # Older version of the data: "https://drive.google.com/open?id=18W7qiSgJI4kBRMX4-KVeHTLp5csVGtle"
+  dataFile <- dataPathGDrive
+  Checksums(path = file.path(getwd(), "data"))
+  filesDownloaded <- preProcess(url = dataFile, 
+                                destinationPath = file.path(getwd(), "data"))
+  filesDownloaded <- filesDownloaded$checkSums$actualFile[grepl(pattern = "rds", 
+                                                                x = filesDownloaded$checkSums$actualFile)]
+  filesDownloadedFull <- file.path(getwd(), "data", filesDownloaded)
+  dt <- lapply(X = filesDownloadedFull, FUN = readRDS)
+  
+  dtClean <- lapply(X = dt, FUN = function(object){
+    if (class(object) != "matrix"){
+      return(object[["Nt"]])
+    } else {
+      return(object)
+    }
+  })
+  
+  herdScenario <- unlist(strsplit(x = filesDownloaded, split = ".rds"))
+  names(dtClean) <- herdScenario
+  
+  PopulationTable <- data.table::rbindlist(lapply(X = herdScenario, FUN = function(herdScen){
+    dt <- data.table::data.table(dtClean[[herdScen]])
+    names(dt) <- paste0("Year", 1837:2075)
+    herdScenMeans <- data.table::rbindlist(lapply(names(dt), function(cl){
+      data.table::data.table(
+        year = sapply(strsplit(cl, split = "Year"), "[", 2 ),
+        #lowerCI = quantile(dt[, get(cl)], 0.05),
+        mean = mean(dt[, get(cl)]),
+        #upperCI = quantile(dt[, get(cl)], 0.95)
+      )
+    }))
+    herdName <- sapply(strsplit(herdScen, split = "_"), "[", 1)
+    scenarioName <- sapply(strsplit(herdScen, split = "_"), "[", 2)
+    herdScenMeans$herd <- herdName
+    herdScenMeans$scenario <- scenarioName
+    return(herdScenMeans)
+  })
+  )
+  if (typeOfFile == "rds"){
+    saveRDS(object = PopulationTable, file = paste0(dataName, ".rds"))
+  } else {
+    if (typeOfFile == "csv")
+      write.csv(x = PopulationTable, file = paste0(dataName, ".csv"))
+  }
+  if (uploadToGoogleDrive)
+    googledrive::drive_upload(media = dataName, path = googledrive::as_id("1LbTDB0JpcdIc_CWrRnhYAUUEJnZ0qS2Y"))
+  return(PopulationTable)
+}
--- R/makePopulationPlot.R
+++ R/makePopulationPlot.R
@@ -1,11 +1,11 @@
 makePopulationPlot <- function(dataPath = file.path(getwd(), "data", "lambdaTableAllScenarios_V3.csv"), # full data path OR R data.table object
-                            startYear = 1837, # Starting year of time series for plotting
+                            startYear = 1845, # Starting year of time series for plotting
                             endYear = 2055,  # Last year of time series for plotting
                             periods = c(1937, 2007), # vector of starting years
                             plotName = file.path(getwd(), "caribouPlot"), # Full path and name of the plot file to be saved without extension
                             #colorCI = c("grey70", "cornflowerblue", "brown1"),
                             #colorLine = c("grey40", "blue3", "red3"),
-                            colorCI = c("#ffeda0", "#feb24c", "#f03b20"),
+                            colorCI = c("#f7f7f7", "#67a9cf", "#ef8a62"),
                             colorLine = c("black", "black", "black"),
                             plotWidth = 1600, 
                             plotHeight = 800,
@@ -61,24 +61,32 @@ makePopulationPlot <- function(dataPath = file.path(getwd(), "data", "lambdaTabl
     facet_grid(scenario ~ herdName) +
     geom_ribbon(data = lambdaTable,
                 aes(x = year, ymin = lowerCI, ymax = upperCI, fill = period)) +
+    labs(x = "Year", y = "Mean lambda") +
     scale_fill_manual(values = colorCI) +
     geom_line(data = lambdaTable[period == "Period1"],
-              aes(x = year, y = mean), color = colorLine[1], size = 1) +
+              aes(x = year, y = mean), color = colorLine[1], size = 1.0) +
     geom_line(data = lambdaTable[period == "Period2"], 
-              aes(x = year, y = mean), color = colorLine[2], size = 1) +
+              aes(x = year, y = mean), color = colorLine[2], size = 1.0) +
     geom_line(data = lambdaTable[period == "Period3"], 
-              aes(x = year, y = mean), color = colorLine[3], size = 1) +
-    geom_line(data = lambdaTable, aes(x = year, y = upperCI, group = 1), color = "black", size = 0.5) + 
-    geom_line(data = lambdaTable, aes(x = year, y = lowerCI, group = 1), color = "black", size = 0.5) +
-    geom_hline(yintercept = 1, color = 'grey60', linetype = "dashed", size = 1) +
+              aes(x = year, y = mean), color = colorLine[3], size = 1.0) +
+    geom_line(data = lambdaTable, aes(x = year, y = upperCI, group = 1), color = "black", size = 1) + 
+    geom_line(data = lambdaTable, aes(x = year, y = lowerCI, group = 1), color = "black", size = 1) +
+    geom_hline(yintercept = 1, color = 'grey60', linetype = "dashed", size = 1.0) +
     theme_bw() +
     theme(legend.position = "none")
   
+  p1 <- p + theme(strip.text.x = element_text(face="bold", size = 18), strip.background.x = element_rect(fill = "white")) +
+    theme(strip.text.y = element_text(face="bold", size = 18), strip.background.y = element_rect(fill = "white")) +
+    theme(axis.text.x = element_text(size = 18, face = "bold")) +
+    theme(axis.text.y = element_text(size = 18, face = "bold")) +
+    theme(axis.title.x = element_text(size = 24, face = "bold")) +
+    theme(axis.title.y = element_text(size = 24, face = "bold"))
+  
   plotName <- paste0(plotName, ".png")
   png(plotName, width = plotWidth, height = plotHeight)
-  print(p)
+  print(p1)
   dev.off()
   if (uploadToGoogleDrive)
     googledrive::drive_upload(media = plotName, path = googledrive::as_id("1LbTDB0JpcdIc_CWrRnhYAUUEJnZ0qS2Y"))
-  return(p)
+  return(p1)
 }
